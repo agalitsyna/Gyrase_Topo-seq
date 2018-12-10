d6183583182b434dfae0176157869a470ca8048c
Aleksandra Galitsyna <agalitzina@gmail.com>
2018-12-10 20:07:13 +0300

    Mapping and parsing tested.

--- Topology/README.md
+++ Topology/README.md
@@ -1,18 +1,23 @@
 ### DNA topology in E. coli
 
-Pipeline for 3C data processing of _E. coli_ from raw reads to .cool and TADs. 
+Pipeline for 3C data processing of _E. coli_ from raw reads (.fastq) to interaction heatmaps (.cool) and TADs annotation. 
 
 #### Initial data description
 
 Two versions of genomes are used: 
 
-- ecolW3110MuSGS.fasta from GEO GSE117186
+- ecolW3110MuSGS.fasta from GEO GSE117186 [1]
 - ecol96.fasta from GenBank (U00096.2 version)
 
-3C data for HpaII restriction enzyme are replicates from GSE107301 (wt, 37C, exponential growth).
+3C data for HpaII restriction enzyme are replicates from GSE107301 (wt, 37C, exponential growth) [2].
 
 #### Processing pipeline
 
 - 00\_download.sh: download and dump of SRA files
 - 01\_mapping.sh: mapping with ```bwa mem``` allowing split read alignment
 - 02\_parse.sh: parsing of sam files with modified version of pairsamtools
+
+
+#### References
+[1] [Single-nucleotide-resolution mapping of DNA gyrase cleavage sites across the Escherichia coli genome.](https://doi.org/10.1093/nar/gky1222)  _Dmitry Sutormin et al._ Nucleic Acids Research, gky1222
+[2] [Multiscale Structuring of the _E. coli_ Chromosome by Nucleoid-Associated and Condensin Proteins.](https://doi.org/10.1016/j.cell.2017.12.027) _Virginia S. Lioy et al._ Cell, Volume 172, Issue 4, 771-783.e18
--- Topology/scripts/02_parse.sh
+++ Topology/scripts/02_parse.sh
@@ -1,17 +1,33 @@
+# Chromosome length count
+#python -c "print(len(''.join([x.strip() for x in open('../data/genome/ecolW3110MuSGS.fasta', 'r').readlines()[1:]])))"
+
 # Cuting the genome with HpaII restriction enzyme
+# Note that for ecolW3110MuSGS.fasta and HpaII enzyme there was a bug of restriction fragments processing!
 #cooler digest -o ../data/genome/ecol96_HpaII.bed ../data/genome/ecol96_chromsizes.txt ../data/genome/ecol96.fasta HpaII
 #cooler digest -o ../data/genome/ecolW3110MuSGS_HpaII.bed ../data/genome/ecolW3110MuSGS_chromsizes.txt ../data/genome/ecolW3110MuSGS.fasta HpaII
 
-# Ordinary parsing with pairsamtools (for example)
+# Ordinary parsing with pairsamtools (example)
 #pairsamtools parse --walks-policy 5unique -c ../data/genome/ecolW3110MuSGS_chromsizes.txt ../data/bam/Sut7.bam | pairsamtools sort --nproc 20 -o ../data/pairsam/Sut7.pairsam
 
 # Parsing with agalitsyna github version of pairsamtools (https://github.com/agalitsyna/pairsamtools)
 # reporting ligation junctions
 
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecolW3110MuSGS_chromsizes.txt --frags ../data/genome/ecolW3110MuSGS_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/Sut7_lj_stats.txt ../data/bam/Sut7_ecolW3110MuSGS.bam > ../data/pairsam/Sut7_ecolW3110MuSGS_lj.pairsam & 
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecolW3110MuSGS_chromsizes.txt --frags ../data/genome/ecolW3110MuSGS_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/SRR70017417_ecolW3110MuSGS_lj_stats.txt ../data/bam/SRR70017417_ecolW3110MuSGS.bam > ../data/pairsam/SRR70017417_ecolW3110MuSGS_lj.pairsam &
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecolW3110MuSGS_chromsizes.txt --frags ../data/genome/ecolW3110MuSGS_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/SRR63545657_ecolW3110MuSGS_lj_stats.txt ../data/bam/SRR63545657_ecolW3110MuSGS.bam > ../data/pairsam/SRR63545657_ecolW3110MuSGS_lj.pairsam &
+enzyme="HpaII"
 
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecol96_chromsizes.txt --frags ../data/genome/ecol96_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/Sut7_ecol96_lj_stats.txt ../data/bam/Sut7_ecol96.bam > ../data/pairsam/Sut7_ecol96_lj.pairsam &
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecol96_chromsizes.txt --frags ../data/genome/ecol96_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/SRR70017417_ecol96_lj_stats.txt ../data/bam/SRR70017417_ecol96.bam > ../data/pairsam/SRR70017417_ecol96_lj.pairsam &
-pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/ecol96_chromsizes.txt --frags ../data/genome/ecol96_HpaII.bed --drop-sam --add-columns rfrag --output-stats ../data/pairsam/SRR63545657_ecol96_lj_stats.txt ../data/bam/SRR63545657_ecol96.bam > ../data/pairsam/SRR63545657_ecol96_lj.pairsam &
+for name in Sut7 SRR7001741 SRR6354565
+do
+	for genome in ecolW3110MuSGS ecol96
+	do 
+		pairsamtools parse --walks-policy ligation_junctions -c ../data/genome/${genome}_chromsizes.txt --frags ../data/genome/${genome}_${enzyme}.bed --drop-sam --add-columns rfrag,rfrag_dist_up,rfrag_dist_down,rfrag_dist --output-stats ../data/pairsam/${name}_${genome}_lj_stats.txt ../data/bam/${name}_${genome}.bam > ../data/pairsam/${name}_${genome}_lj.pairsam &	
+	done
+done
+
+wait
+
+for name in Sut7 SRR7001741 SRR6354565
+do
+	for genome in ecolW3110MuSGS ecol96
+	do 
+		pairsamtool dedup --output-stats ../data/pairsam/${name}_${genome}_lj_stats.txt ../data/pairsam/${name}_${genome}_lj.pairsam > ../data/pairsam/${name}_${genome}_lj.pairsam.dedup
+	done
+done
